a3b7bbe29df9df3ff82d393305686630f98c8f8b
Cathunder <102372626+Cathunder@users.noreply.github.com>
2024-09-16 22:02:23 +0900

    [Silver V] Title: 단어 정렬, Time: 488 ms, Memory: 24160 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,27 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = Integer.parseInt(br.readLine());
 
-        String[] arr = new String[cnt];
-        for(int i = 0; i < cnt; i++) {
-            arr[i] = br.readLine();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < cnt; i++) {
+            set.add(br.readLine());
         }
 
-        Arrays.sort(arr);
-        Arrays.sort(arr, Comparator.comparingInt(String::length));
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            if(!arr[i].equals(arr[i + 1])) {
-                System.out.println(arr[i]);
-            }
-        }
-        System.out.println(arr[arr.length - 1]);
+        List<String> words = new ArrayList<>(set);
+        words.stream()
+            .sorted(Comparator.comparingInt(String::length).thenComparing((String::compareTo)))
+            .forEach(System.out::println);
     }
 }
